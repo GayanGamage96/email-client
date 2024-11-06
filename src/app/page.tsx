f6fbac9b20155b6da5cca77dda13c953a71db7c3
GayanGamage96 <gayangamagercg1996@gmail.com>
2024-11-06 22:11:51 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  return (
+    <div>
+      <h1 className="text-red-600">hello world</h1>
+      <Button>Hello world</Button>
+    </div>
+  );
+}
